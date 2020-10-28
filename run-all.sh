f2cddb8bb5db32791d90d27b0ae55b42e154811f
duane_hinkley <duane.hinkley@mheducation.com>
2020-10-28 09:20:24 -0700

    Cleaning up and standardizing investigations

--- run-all.sh
+++ run-all.sh
@@ -2,26 +2,25 @@
 
 cd jupter
 
-#runipy 01-summarization.ipynb
-jupyter nbconvert --to notebook --inplace --execute 01-summarization.ipynb
+runipy 01-summarization.ipynb
 
 for file in `ls -1 summary/*.ipynb`
 do
   echo "========================================================="
   echo "Running $file"
   echo "========================================================="
-  jupyter nbconvert --to notebook --inplace --execute $file
+  runipy $file
 done
 
-jupyter nbconvert --to notebook --inplace --execute 02-investigation.ipynb
+runipy 02-investigation.ipynb
 
 for file in `ls -1 investigation/*.ipynb`
 do
   echo "========================================================="
   echo "Running $file"
   echo "========================================================="
-  jupyter nbconvert --to notebook --inplace --execute $file
+  runipy $file
 done
 
 
-jupyter nbconvert --to notebook --inplace --execute 03-cleaning.ipynb
+runipy 03-cleaning.ipynb
